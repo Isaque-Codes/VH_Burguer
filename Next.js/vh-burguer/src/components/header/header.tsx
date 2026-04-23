e6877296374647b35e72c28a90174472f0529714
Isaque-Codes <isaque.137.silva@gmail.com>
2026-04-23 08:42:22 -0300

    ✨ feat: Finalização da tela home com os componentes card-produto e lista-produto

--- Next.js/vh-burguer/src/components/header/header.tsx
+++ Next.js/vh-burguer/src/components/header/header.tsx
@@ -1,4 +1,5 @@
 import styles from "./header.module.css";
+import Link from "next/link";
 
 const Header = () => {
     return (
@@ -6,10 +7,10 @@ const Header = () => {
             <div id={styles.container} className="Layout_guide">
                 <img src="../imgs/logo_VH_Burguer.svg" alt="Logo do VH Burguer que contém um hamburguer como plano de fundo" id={styles.logo}/>
                 <nav id={styles.nav_menu}>
-                    <a href="">Destaques</a>
-                    <a href="">Cardápio</a>
-                    <a href="">Unidades</a>
-                    <a href="">Login</a>
+                    <a href="#destaques">Destaques</a>
+                    <a href="#cardapio">Cardápio</a>
+                    <a href="#unidades">Unidades</a>
+                    <Link href="/login">Login</Link>
                 </nav>
                 <button id={styles.btn_icon}>
                     <img src="../imgs/icon_hamburguer.svg" alt="Ícone que representa um hamburguer para acessar o menu lateral" />
